b4c7ee596ed3a1ebc5aed6bb2d36468cffe37fa5
Miguel <90362181+miguelbrazz@users.noreply.github.com>
2021-11-22 12:32:32 -0300

    TelaQRCode
    ATUALIZADA !

--- TelaQRCode/App.js
+++ TelaQRCode/App.js
@@ -1,45 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-
+import React, { useState } from 'react';
+import { SafeAreaView, Text, View, StyleSheet, TextInput, Button, TouchableOpacity, Image } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [qrValue, setQrValue] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.textqrcode}>Foque no QR Code dentro da moldura{"\n"}para escanear</Text>
-      <StatusBar style="auto" />
-
-      <View style={{width:265, height:265, backgroundColor: '#F2F2F2'}} />
-
-      <TouchableOpacity style={styles.btnVoltar}>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.text}>
+        Foque no QR Code dentro da moldura{"\n"}para escanear</Text>
+      <View style={styles.containerQr}>
+        <QRCode
+          value={qrValue ? qrValue : 'NA'}
+          size={185}
+          color="black"
+          backgroundColor="white"
+        />
+      </View>
+        <TouchableOpacity style={styles.btnVoltar}>
           <Image style={{width:60,height:60}} source={require('./assets/botaovoltar.png')} />
-      </TouchableOpacity>
-
-      <Text style={styles.btnCancelar} >Cancelar</Text>
-    </View>
+          <Text style={styles.btnCancelar} >Cancelar</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#DCDCDC',
+    backgroundColor: '#E6E6E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
-  textqrcode: {
+  containerQr: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 45,
+    borderColor: 'white'
+  },
+
+  text: {
     textAlign: 'center',
     fontSize:16,
     opacity:.70,
-    marginBottom:15
-  },
+    marginBottom:18
 
+  },
+  textInput: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    margin: 10,
+  },
   btnVoltar: {
-    marginTop:70
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:60
   },
 
   btnCancelar: {
+    justifyContent: 'center',
     opacity:.60,
     fontSize:17
   }
