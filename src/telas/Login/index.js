bde28ceeedbb8fefd620939ed7c99daa3788ec20
tiagowl <winkellandi123@gmail.com>
2021-12-08 18:58:31 -0300

    commit final

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -15,7 +15,7 @@ export default function App({navigation}) {
     try{
       const response = await api.post("/usuario/login", {cpf: email, senha});
       setLogado(1);
-      navigation.navigate("Home", {userId: response.data.id});
+      navigation.navigate("Home");
       // navigation.navigate("Home");
       console.log(response)
     }catch(error){
@@ -35,12 +35,12 @@ export default function App({navigation}) {
         <Text style={styles.label} >Senha</Text>
         <TextInput onChangeText={text=>setSenha(text)} style={styles.input} />
         {logado === 2 &&<Text style={[styles.label, {color: "red", marginTop: 10}]} >Email ou Senha incorretos.</Text>}
-        {/* <TouchableOpacity style={styles.btnEntry} onPress={Login} >
-          <Text style={styles.btnEntryText} >Entrar</Text>
-        </TouchableOpacity> */}
-        <TouchableOpacity style={styles.btnEntry} onPress={()=>navigation.navigate("Home")} >
+        <TouchableOpacity style={styles.btnEntry} onPress={Login} >
           <Text style={styles.btnEntryText} >Entrar</Text>
         </TouchableOpacity>
+        {/* <TouchableOpacity style={styles.btnEntry} onPress={()=>navigation.navigate("Home")} >
+          <Text style={styles.btnEntryText} >Entrar</Text>
+        </TouchableOpacity> */}
       </View>
       <View style={styles.whiteSpace} >
         <TouchableOpacity style={styles.btnNew} onPress={()=> navigation.navigate("Cadastro") } >
